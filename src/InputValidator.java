85fa6fb0860a2f76eaf35cded07a78ae65249f05
Kenneth Torcuator <torcuatorkenneth6@gmail.com>
2025-09-26 22:21:31 +0800

    New Repos

---
+++ src/InputValidator.java
@@ -0,0 +1,33 @@
+import  java.util.Scanner;
+
+public class InputValidator {
+    private static final Scanner scanner = new Scanner(System.in);
+
+      public static int getIntInput(String prompt) {
+        int number;
+        while (true) {
+            System.out.print(prompt);
+            String input = scanner.nextLine();
+            try {
+                number = Integer.parseInt(input);
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid number, please try again.");
+            }
+        }
+        return number;
+    }
+    public static String getStringInput(String prompt) {
+        String input;
+        while (true) {
+            System.out.print(prompt);
+            input = scanner.nextLine().trim();
+            if (input.matches("[a-zA-Z]+")) {
+                break;
+            } else {
+                System.out.println("Invalid input, please try again.");
+            }
+        }
+        return input;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,22 @@
+public class Main {
+
+    public static void main(String[] args) {
+        studentManager student = new studentManager();
+        boolean running = true;
+        while (running) {
+            student.printMenu();
+            int choice = InputValidator.getIntInput("Enter your choice: ");
+
+            switch (choice) {
+                case 1 -> student.addStudent();
+                case 2 -> student.viewAllStudents();
+                case 3 -> {
+                    System.out.println("Exiting program. Goodbye!");
+                    running = false;
+                }
+                default -> System.out.println("Invalid choice, try again.");
+            }
+
+        }
+    }
+}
