a26b915cd8ba17f378fc5a04328b78e8363c22eb
Kenneth Torcuator <torcuatorkenneth6@gmail.com>
2025-10-27 15:13:52 +0800

    Added a method for validation of empty input for updating student profile

--- students_profile_maven/src/main/java/com/students/InputValidator.java
+++ students_profile_maven/src/main/java/com/students/InputValidator.java
@@ -24,7 +24,7 @@ public class InputValidator {
         while (true) {
             System.out.print(prompt);
             input = scanner.nextLine().trim();
-            if (input.matches("[a-zA-Z ]+")) {  // <-- added a space inside the brackets
+            if (input.matches("[-a-zA-Z-0-9 ]+")) {  // <-- added a space inside the brackets
                 break;
             } else {
                 System.out.println("Invalid input, please try again.");
@@ -32,5 +32,33 @@ public class InputValidator {
         }
         return input;
 }
+    public static String getBlankStringInput(String prompt) {
+        System.out.print(prompt);
+        return scanner.nextLine().trim();
+    }
+    public static int getBlankIntInput(String prompt) {
+        System.out.print(prompt);
+        String input = scanner.nextLine().trim();
+        if (input.isEmpty()) {
+            return -1; // Indicate no change
+        }
+        try {
+            return Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            System.out.println("Invalid number, please try again.");
+            while (true){
+                System.out.print(prompt);
+                input = scanner.nextLine().trim();
+                if (input.isEmpty()) {
+                    return -1; // Indicate no change
+                }
+                try {
+                    return Integer.parseInt(input);
+                } catch (NumberFormatException ex) {
+                    System.out.println("Invalid number, please try again.");
+                }
+            }
+        }
+    }
 
 }
